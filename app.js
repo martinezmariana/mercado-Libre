5ae721aff398fa000cf89e8cf37bcc6239c4b63c
martinezmariana <maruayemartiiz2018@gmail.com>
2022-12-12 18:52:54 -1100

    agregando mis codigos

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3001, () => {
+  console.log("Se prendió");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
